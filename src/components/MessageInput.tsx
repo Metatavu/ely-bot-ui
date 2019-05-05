df5ab7da09768959b551d71ab442cd79ebca4af5
Antti Leppä <antti.leppa@metatavu.fi>
2019-05-05 10:08:43 +0300

    Fixed isses with mobile layout

--- src/components/MessageInput.tsx
+++ src/components/MessageInput.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import * as _ from "lodash";
-import { Button, Grid, Input, InputOnChangeData, Container } from "semantic-ui-react";
+import { Button, Grid, Input, InputOnChangeData, Container, Popup, ButtonProps } from "semantic-ui-react";
 
 import 'react-image-lightbox/style.css';
 import '../styles/message-input.css'
 
-export interface Props {
+/**
+ * Component props
+ */
+interface Props {
   conversationStarted: boolean,
   waitingForBot: boolean,
   hint: string,
@@ -15,21 +18,54 @@ export interface Props {
   onRestartConversation: () => void
 }
 
-export interface State {
+/**
+ * Component state
+ */
+interface State {
   pendingMessage: string
+  globalQuickResponsesMenuOpen: boolean
 }
 
+/**
+ * MessageInput component
+ */
 class MessageInput extends React.Component<Props, State> {
 
+  private windownMousedownListener: () => void;
   private messagesEnd: any;
 
+  /**
+   * Constructor
+   * 
+   * @param props component props 
+   */
   constructor(props: Props) {
     super(props);
     this.state = { 
-      pendingMessage: ""
+      pendingMessage: "",
+      globalQuickResponsesMenuOpen: false
     };
+
+    this.windownMousedownListener = this.onWindowMousedown.bind(this);
   }
 
+  /**
+   * Component did mount life-cycle event
+   */
+  public componentDidMount() {
+    window.addEventListener('mousedown', this.windownMousedownListener);
+  }
+  
+  /**
+   * Component will unmount life-cycle event
+   */
+  public componentWillUnmount() {
+    window.removeEventListener('mousedown', this.windownMousedownListener);
+  }
+
+  /**
+   * Component render method
+   */
   public render() {
     return (
       <div className="message-input-container">
@@ -49,7 +85,7 @@ class MessageInput extends React.Component<Props, State> {
                   <Button className="message-send" disabled={this.props.waitingForBot} onClick={this.onSendButtonClick}></Button>
                 </Grid.Column>
               </Grid.Row>
-              <Grid.Row style={{ paddingBottom: 0 }}>
+              <Grid.Row style={{ paddingTop: 0 }}>
                 <Grid.Column>
                   { this.renderGlobalQuickResponses() }
                 </Grid.Column>
@@ -57,7 +93,7 @@ class MessageInput extends React.Component<Props, State> {
               <Grid.Row style={{ paddingTop: 0 }}>
                 <Grid.Column>
                   <a className="powered-by" target="_blank" href="https://www.metamind.fi">
-                    Powered by Metamind - a chatbot from Metatavu Oy
+                    Powered by Metamind - a chatbot from Metatavu&nbsp;Oy
                   </a>
                 </Grid.Column>
               </Grid.Row>
@@ -75,14 +111,31 @@ class MessageInput extends React.Component<Props, State> {
     if (!this.props.globalQuickResponses.length) {
       return null;
     }
+    
+    if (window.innerWidth > 768) {
+      return this.renderGlobalQuickResponseButtons();
+    }
 
+    return (
+      <Popup trigger={<Button className="global-quick-responses-open" content="Pikavalinnat" onClick={ this.onGlobalQuickResponsePopupLinkClick }/> }
+        content={ this.renderGlobalQuickResponseButtons() }
+        on='click'        
+        position='top right'
+        open={ this.state.globalQuickResponsesMenuOpen }
+      />
+    );
+  }
+
+  /**
+   * Renders global quick reponse buttons
+   */
+  private renderGlobalQuickResponseButtons() {
     return (
       <div className="global-quick-responses">
         { this.props.globalQuickResponses.map((globalQuickResponse) => {
-          return <Button className="global-quick-response" onClick={() => this.props.onSendMessage(globalQuickResponse)}>{ globalQuickResponse }</Button>  
-        }) }
-      </div>
-    );
+          return <Button className="global-quick-response" onClick={() => this.onGlobalQuickResponseClick(globalQuickResponse)}>{ globalQuickResponse }</Button>  
+      }) }
+    </div>)
   }
   
   private scrollToBottom = () => {
@@ -91,6 +144,34 @@ class MessageInput extends React.Component<Props, State> {
     }
 
     this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+  }
+  
+  /**
+   * Event handler for window mouse down event
+   */
+  private onWindowMousedown = () => {
+    this.setState({
+      globalQuickResponsesMenuOpen: false
+    });
+  }
+
+  /**
+   * Event handler for global quick response popup open button click
+   */
+  private onGlobalQuickResponsePopupLinkClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, data: ButtonProps) => {
+    event.preventDefault();
+    this.setState({ globalQuickResponsesMenuOpen: true });    
+  }
+
+  /**
+   * Event handler for global quick response button click
+   */
+  private onGlobalQuickResponseClick = (globalQuickResponse: string) => {
+    this.setState({
+      globalQuickResponsesMenuOpen: false
+    });
+
+    this.props.onSendMessage(globalQuickResponse);
   }
 
   private onPendingMessageChange = (e: any, data: InputOnChangeData) => {
